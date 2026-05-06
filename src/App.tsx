3db8ee983f301fe365c7c9bb2c3064fd5db173a3
KaioMorais41 <kaiomorais2903@email.com>
2026-05-06 13:30:56 -0300

    feat: adiciona Navbar, Footer e configuração do Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import Home from "./pages/home/home"
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./footer/Footer"
+import Home from "./pages/home/Home"
+
 
 
 function App() {
 
   return (
+    <>
+    <Navbar/>
     <Home/>
-   
-  )
+    <Footer/>
+    </>   
+  );
 }
 
 export default App
